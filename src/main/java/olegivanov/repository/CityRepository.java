ec28cb3760c740d73c1cd7e87aba654e052f5561
Black Smith-JD-40 <ol.channel.t@gmail.com>
2023-09-22 02:56:06 +0400

    Ready for review/cleaned

--- src/main/java/olegivanov/repository/CityRepository.java
+++ src/main/java/olegivanov/repository/CityRepository.java
@@ -11,6 +11,5 @@ import java.util.Optional;
 public interface CityRepository extends JpaRepository<City,Integer> {
     List<City> findByNameIsLike(String name);
 
-
     List<City> findByName(String name);
 }
